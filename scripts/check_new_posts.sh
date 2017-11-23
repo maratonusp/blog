bc54de216b44b2020d4d76832cff50b92e4ff3ab
Yan Couto <yancouto@gmail.com>
2017-11-23 17:35:31 -0200

    Posts na página automatico

--- scripts/check_new_posts.sh
+++ scripts/check_new_posts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for f in `git diff HEAD^ --name-only --diff-filter=A --relative=_posts`; do
-	./post_to_bot.py $f
+	./post_to_world.py $f $MARATONIME_HELPER_TOKEN $MARATONIME_PAGE_TOKEN
 done
---
+++ scripts/post_to_world.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import sys
+import requests
+import yaml
+import re
+
+print("Posting file " + sys.argv[1])
+
+def find_kth(string, pat, k):
+	fst = string.find(pat)
+	if k == 1:
+		return fst
+	else:
+		return find_kth(string[fst + 1:], pat, k - 1) + fst + 1
+
+txt = open('../_posts/' + sys.argv[1], 'r').read();
+header = txt[txt.find('\n') + 1 : find_kth(txt, '---\n', 2)]
+text = txt[find_kth(txt, '---\n', 2) + 4:].strip()
+info = yaml.load(header)
+link = 'https://www.ime.usp.br/~maratona/'
+try:
+	permalink = info['permalink'].strip(' "/\n');
+	if permalink.find(':') == -1:
+		link += permalink
+	elif permalink == ':categories/:title':
+		raise 'default'
+except:
+	for c in info['categories']:
+		link += c + '/'
+	fn = sys.argv[1]
+	for i in range(3):
+		fn = fn[fn.find('-') + 1:]
+	link += fn[:fn.rfind('.')]
+
+text_channel = "Novo post no blog: '[" + info['title'] + "](" + link + ")"
+parag = text[:text.find('\n\n')]
+parag = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', parag)
+text_page = info['title'] + '\n\n' + parag.rstrip() + '\n\nLeia mais no site. :)'
+
+token_bot = sys.argv[2]
+token_page = sys.argv[3]
+
+requests.post('https://api.telegram.org/bot' + token_bot + '/sendMessage', data={'chat_id': '@maratonime', 'text': text_channel, 'parse_mode': 'Markdown'})
+
+requests.post('https://graph.facebook.com/v2.11/maratonIME/feed', data={'message': text_page, 'access_token': token_page, 'link': link})
