ed97afceb59751ea89fe588b6af934726f9d0fd0
Yan Couto <yancouto@gmail.com>
2017-11-23 18:57:48 -0200

    Adicionando comentários

--- scripts/check_new_posts.sh
+++ scripts/check_new_posts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Getting the name of all files in the directory `_posts/` that were added in the last commit
 for f in `git diff HEAD^ --name-only --diff-filter=A --relative=_posts`; do
 	./post_to_world.py $f $MARATONIME_HELPER_TOKEN $MARATONIME_PAGE_TOKEN
 done
--- scripts/post_to_world.py
+++ scripts/post_to_world.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 import sys
+
+# Parameters
+# 1. filename (for the .md in _posts/ folder)
+filename = sys.argv[1]
+# 2. Bot token to post the message to the channel
+token_bot = sys.argv[2]
+# 3. Page token to post the message to the facebook page
+token_page = sys.argv[3]
+
+
 import requests
 import yaml
 import re
 
 print("Posting file " + sys.argv[1])
 
+# Find the position of the k-th occurrence of pat in string
 def find_kth(string, pat, k):
 	fst = string.find(pat)
 	if k == 1:
@@ -13,10 +24,12 @@ def find_kth(string, pat, k):
 	else:
 		return find_kth(string[fst + 1:], pat, k - 1) + fst + 1
 
-txt = open('../_posts/' + sys.argv[1], 'r').read();
+txt = open('../_posts/' + filename, 'r').read();
 header = txt[txt.find('\n') + 1 : find_kth(txt, '---\n', 2)]
 text = txt[find_kth(txt, '---\n', 2) + 4:].strip()
 info = yaml.load(header)
+
+# Creating link from post header
 link = 'https://www.ime.usp.br/~maratona/'
 try:
 	permalink = info['permalink'].strip(' "/\n');
@@ -27,19 +40,19 @@ try:
 except:
 	for c in info['categories']:
 		link += c + '/'
-	fn = sys.argv[1]
+	fn = filename
 	for i in range(3):
 		fn = fn[fn.find('-') + 1:]
 	link += fn[:fn.rfind('.')]
 
 text_channel = "Novo post no blog: '[" + info['title'] + "](" + link + ")"
+r = requests.post('https://api.telegram.org/bot' + token_bot + '/sendMessage', data={'chat_id': '@maratonime', 'text': text_channel, 'parse_mode': 'Markdown'})
+print("Bot response: " + str(r.status_code) + " " + r.reason)
+
+# First paragraph
 parag = text[:text.find('\n\n')]
+# Removing markdown links
 parag = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', parag)
 text_page = info['title'] + '\n\n' + parag.rstrip() + '\n\nLeia mais no site. :)'
-
-token_bot = sys.argv[2]
-token_page = sys.argv[3]
-
-requests.post('https://api.telegram.org/bot' + token_bot + '/sendMessage', data={'chat_id': '@maratonime', 'text': text_channel, 'parse_mode': 'Markdown'})
-
-requests.post('https://graph.facebook.com/v2.11/maratonIME/feed', data={'message': text_page, 'access_token': token_page, 'link': link})
+r = requests.post('https://graph.facebook.com/v2.11/maratonIME/feed', data={'message': text_page, 'access_token': token_page, 'link': link})
+print("Bot response: " + str(r.status_code) + " " + r.reason)
