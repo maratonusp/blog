7ff8170e1d863e741faab197eb3f7fc7c68a887a
Felipe Noronha <felipe.castro.noronha@hotmail.com>
2021-06-04 21:12:57 -0300

    change AND to -e argument

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -21,4 +21,4 @@ rsync -e "sshpass -p $PSWD ssh -o StrictHostKeyChecking=no" _site/ $USER:$DEST -
 
 # Envia o arquivo para o servidor final
 echo -e "\033[0;32m[Copiando arquivos para o servidor final]\033[0m"
-sshpass -p $PSWD ssh -o StrictHostKeyChecking=no $USER "ksu maratona && rsync $DEST /home/specmac/maratona/www -r"
+sshpass -p $PSWD ssh -o StrictHostKeyChecking=no $USER "ksu maratona -e rsync $DEST /home/specmac/maratona/www -r"
